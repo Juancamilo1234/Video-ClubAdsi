dad660828cf3e0bceab9770d2f6bfca0a1cfc7e0
juancamilo1234 <juancamilo963852@hotmail.es>
2017-08-25 03:30:23 -0500

    avance 1

---
+++ client/app/usuarios/usuarios-create/usuarios-create.controller.js
@@ -0,0 +1,52 @@
+'use strict';
+
+(function(){
+
+class UsuariosCreateComponent {
+  constructor(ciudadesService,departamentosService) {
+    this.ciudadesService = ciudadesService;
+    this.departamentosService = departamentosService;
+
+    this.selectedItem  = null;
+    this.searchText    = null;
+  }
+
+  $onInit(){
+
+  	this.departamentosService.query().$promise
+  	.then(response => {
+  		this.departamentos = response;
+  	})
+  	.catch(err => console.log(err));
+
+  }
+  getCiudad(){
+  	console.log(this.idDepartamento);
+    this.ciudadesService.getCiudades({idDepartamento:this.idDepartamento}).$promise
+    .then(response => {
+      console.log("Ciudades",response);
+      this.ciudades = response;
+    })
+    .catch(err => console.error(err));
+  }
+
+  guardarUsuario(){
+  	console.log(this.usuario);
+  }
+  querySearch(dato){
+    return this.ciudadesService.getCiudades({nombre:dato}).$promise
+    .then(response =>{
+      return response;
+    })
+
+  }
+}
+
+angular.module('videoClubApp')
+  .component('usuariosCreate', {
+    templateUrl: 'app/usuarios/usuarios-create/usuarios-create.html',
+    controller: UsuariosCreateComponent,
+    controllerAs: 'vm'
+  });
+
+})();
